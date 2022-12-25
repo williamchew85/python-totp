c50b082a66955099e09fdf5c7f5d0e2dcd7666c8
williamchew85 <williamchew85@gmail.com>
2022-12-26 02:25:27 +0800

    Update totp.py

--- totp.py
+++ totp.py
@@ -2,13 +2,24 @@
 # It then calls the generate() method on the totp object to generate the OTP, and prints the OTP to stdout.
 
 # Make sure that the totp.py script is in the same directory as the totp.py module that contains the TOTP class, or that the module is importable from the sys.path in the totp.py script.import json
+import json
 import sys
+import hashlib
 
 from totp import TOTP
 
+def generate_secret(email):
+  # Hash the email address to create a unique secret
+  secret = hashlib.sha256(email.encode()).hexdigest()
+  return secret
+
 # Read the data from stdin
 data = json.loads(sys.stdin.read())
-secret = data['secret']
+email = data['email']
+
+# Generate the secret for the email address
+# (e.g. using a hashing function or a database lookup)
+secret = generate_secret(email)
 
 # Create a TOTP object
 totp = TOTP(secret)
